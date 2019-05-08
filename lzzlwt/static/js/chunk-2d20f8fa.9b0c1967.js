ee676323137df9ec3912f49aa9f5fe918438ef01
lzz <375621354@qq.com>
2019-05-08 10:19:14 +0800

    yarn install

--- lzzlwt/static/js/chunk-2d20f8fa.9b0c1967.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d20f8fa"],{b3c6:function(n,e,c){"use strict";c.r(e);var t={name:"person"},r=c("2877"),s=Object(r.a)(t,function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("person")])},[],!1,null,"2947dc93",null);e.default=s.exports}}]);
--- lzzlwt/static/js/chunk-2d224c94.211274d1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d224c94"],{e22c:function(n,e,c){"use strict";c.r(e);var t={name:"Gong"},u=c("2877"),a=Object(u.a)(t,function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n    公共页面\n")])},[],!1,null,"075f7ff4",null);e.default=a.exports}}]);
